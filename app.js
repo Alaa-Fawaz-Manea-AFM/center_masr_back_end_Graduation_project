df5555599cac7a265a361ea1ca3bdfc6c6445337
Alaa-fawaz-Manea-AF <alaafawaz010011@gmail.com>
2026-04-07 10:45:08 +0200

    update app.js

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(
 const allowedOrigins =
   NODE_ENV === "production"
     ? [CLIENT_URL]
-    : ["http://localhost:3000", "http://127.0.0.1:3000"];
+    : ["http://localhost:3000", `http://0.0.0.0:${PORT}`];
 
 app.use(
   cors({
